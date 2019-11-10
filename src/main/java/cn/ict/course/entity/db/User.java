e91c669143f9d9a74057c45a9006993c9c19a748
iIQOverflow <fengjianyongcn@gmail.com>
2019-11-10 15:02:52 +0800

    简化了用户表，管理员、学生、老师使用一张用户表存储信息。 修改了课程表的一些字段。

--- src/main/java/cn/ict/course/entity/db/User.java
+++ src/main/java/cn/ict/course/entity/db/User.java
@@ -1,30 +1,22 @@
 package cn.ict.course.entity.db;
 
 import lombok.Data;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 /**
- * @author 965087276@qq.com
- * @date 2019/11/9 14:11
- */
+ * @author Jianyong Feng
+ **/
 @Entity
-@Table(name = "student")
+@Table(name = "user")
 @Data
-public class Student extends BaseEntity {
+public class User extends BaseEntity {
     /**
-     * 学号
+     * 工号
      */
-    @Column(name = "student_code", nullable = false)
-    private String studentCode;
-
-    /**
-     * 密码
-     */
-    @Column(name = "password", nullable = false)
-    private String password;
+    @Column(name = "user_code", nullable = false)
+    private String userCode;
 
     /**
      * 姓名
@@ -42,7 +34,19 @@ public class Student extends BaseEntity {
      * 手机号
      */
     @Column(name = "phone_number", nullable = false)
+
     private String phoneNumber;
+    /**
+     * 密码
+     */
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    /**
+     * 盐
+     */
+    @Column(name = "salt", nullable = false)
+    private String salt;
 
     /**
      * 学院
@@ -51,9 +55,20 @@ public class Student extends BaseEntity {
     private String college;
 
     /**
+     * 单位
+     */
+    @Column(name = "unit", nullable = false)
+    private String unit;
+
+    /**
      * 专业
      */
     @Column(name = "major", nullable = false)
-    private double major;
+    private String major;
 
+    /**
+     * 类型
+     */
+    @Column(name = "type", nullable = false)
+    private String type;
 }
