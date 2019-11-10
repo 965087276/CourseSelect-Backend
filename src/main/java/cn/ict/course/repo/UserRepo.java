63c2315707825504d1440715a7a4e4b5bab3cc28
iIQOverflow <fengjianyongcn@gmail.com>
2019-11-10 14:51:18 +0800

    简化了用户表，管理员、学生、老师使用一张用户表存储信息。

---
+++ src/main/java/cn/ict/course/repo/UserRepo.java
@@ -0,0 +1,14 @@
+package cn.ict.course.repo;
+
+import cn.ict.course.entity.db.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Jianyong Feng
+ **/
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+
+}
