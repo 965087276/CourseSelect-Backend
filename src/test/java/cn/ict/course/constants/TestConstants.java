e308ad0b2a30c9dfe293bde45ea42f7279ec9cdd
iIQOverflow <fengjianyongcn@gmail.com>
2019-12-03 21:03:09 +0800

    用户保存测试。

---
+++ src/test/java/cn/ict/course/constants/TestConstants.java
@@ -0,0 +1,19 @@
+package cn.ict.course.constants;
+
+/**
+ * @author Jianyong Feng
+ **/
+public class TestConstants {
+    public static final String TEST_USER_INFO = "{" +
+            "  \"college\": \"网络空间安全学院\"," +
+            "  \"email\": \"90388@qq.com\"," +
+            "  \"major\": \"网络安全技术\"," +
+            "  \"password\": \"123456\"," +
+            "  \"phoneNumber\": \"18106275581\"," +
+            "  \"realName\": \"乔布斯\"," +
+            "  \"role\": \"student\"," +
+            "  \"unit\": \"信息工程研究所\"," +
+            "  \"username\": \"201903\"" +
+            "}";
+
+}
--- src/test/java/cn/ict/course/controller/UserControllerTest.java
+++ src/test/java/cn/ict/course/controller/UserControllerTest.java
@@ -1,10 +1,44 @@
 package cn.ict.course.controller;
 
+import cn.ict.course.service.UserService;
+import cn.ict.course.service.impl.UserServiceImpl;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import static cn.ict.course.constants.TestConstants.TEST_USER_INFO;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class UserControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mvc;
+    private MockHttpSession mockHttpSession;
+
+    @Before
+    public void setupMockMvc() {
+        mvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
 
     @Test
     public void login() {
@@ -12,8 +46,12 @@ public class UserControllerTest {
     }
 
     @Test
-    public void save() {
-
+    public void save() throws Exception {
+        mvc.perform(post("/xk/api/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(TEST_USER_INFO))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
     }
 
     @Test
