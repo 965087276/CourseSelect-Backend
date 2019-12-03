5c40605a6c43f619b34b11ddeeebeb0461670e4e
wangjinhao <965087276@qq.com>
2019-12-03 16:27:25 +0800

    添加我的预选课接口

--- src/main/java/cn/ict/course/entity/vo/MyPreCourseVO.java
+++ src/main/java/cn/ict/course/entity/vo/MyPreCourseVO.java
@@ -1,8 +1,39 @@
 package cn.ict.course.entity.vo;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.ict.course.entity.bo.MyPreCourseBO;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author 965087276@qq.com
  * @date 2019/12/3 16:10
  */
+@Data
 public class MyPreCourseVO {
+    private String courseCode;
+    private String courseName;
+    private double predit;
+    private String courseType;
+    private String courseTeacher;
+    private boolean addToTable;
+    List<Schedule> schedules = new ArrayList<>();
+
+    @Data
+    class Schedule {
+        private int startWeek;
+        private int time;
+        private int endWeek;
+        private int day;
+        private String classroom;
+    }
+
+    public void addSchedule(MyPreCourseBO bo) {
+        Schedule schedule = BeanUtil.toBean(bo, Schedule.class);
+        schedules.add(schedule);
+    }
 }
+
+
--- src/main/java/cn/ict/course/mapper/CourseMapper.java
+++ src/main/java/cn/ict/course/mapper/CourseMapper.java
@@ -1,8 +1,35 @@
 package cn.ict.course.mapper;
 
+import cn.ict.course.entity.bo.MyPreCourseBO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 /**
  * @author 965087276@qq.com
  * @date 2019/12/3 15:55
  */
+@Mapper
+@Repository
 public interface CourseMapper {
+
+    @Select("SELECT A.course_name courseName," +
+            "       A.course_code courseCode," +
+            "       A.course_teacher courseTeacher," +
+            "       A.course_type courseType," +
+            "       A.credit credit," +
+            "       B.start_week startWeek," +
+            "       B.end_week endWeek," +
+            "       B.day day," +
+            "       B.time time," +
+            "       B.add_to_table addToTable" +
+            "       B.classroom classroom" +
+            "FROM course_schedule B " +
+            "LEFT JOIN course A on A.course_code = B.course_code " +
+            "LEFT JOIN course_preselect C on C.course_code = B.course_code " +
+            "WHERE C.username = #{username}")
+    List<MyPreCourseBO> listMyPreCourse(@Param("username") String username);
 }
