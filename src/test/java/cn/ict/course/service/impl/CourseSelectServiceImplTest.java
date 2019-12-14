b8da24b3f40b9e0d50907ed863c3dd7653db35bf
iIQOverflow <fengjianyongcn@gmail.com>
2019-12-14 17:02:57 +0800

    getConflictedCourseCode测试

--- src/test/java/cn/ict/course/service/impl/CourseSelectServiceImplTest.java
+++ src/test/java/cn/ict/course/service/impl/CourseSelectServiceImplTest.java
@@ -2,7 +2,6 @@ package cn.ict.course.service.impl;
 
 import cn.ict.course.constants.CourseConflictConst;
 import cn.ict.course.entity.bo.GradesInfoBO;
-import cn.ict.course.entity.db.CourseSelect;
 import cn.ict.course.entity.db.User;
 import cn.ict.course.entity.http.ResponseEntity;
 import cn.ict.course.service.CourseSelectService;
@@ -47,17 +46,18 @@ public class CourseSelectServiceImplTest {
             "  \"username\": \"test_user123456\"\n" +
             "}", User.class);
 
-    private final String testUsername = testUser.getUsername();
-    private final String testCourseCode = "UCASO5KBI075388";
-    private final String startTime = "2019-12-14T07:40:05.053Z";
-    private final String endTime = "2019-12-14T07:40:05.052Z";
+    private final String TEST_USERNAME = testUser.getUsername();
+    private final String TEST_COURSE_CODE = "UCASO5KBI075388";
+    private final String TEST_CONFLICTED_COURSE_CODE = "UCAS4TPXD751085";
+    private final String START_TIME = "2019-12-14T07:40:05.053Z";
+    private final String END_TIME = "2019-12-14T07:40:05.052Z";
 
     @Before
     public void setUp() {
         log.info("Test Start");
         ResponseEntity responseEntity = userService.save(testUser);
         assertEquals(STATUS_OK, responseEntity.getStatus());
-        courseSelectService.deleteCourseSelected(testCourseCode, testUsername);
+        courseSelectService.deleteCourseSelected(TEST_COURSE_CODE, TEST_USERNAME);
     }
 
     @After
@@ -69,26 +69,26 @@ public class CourseSelectServiceImplTest {
 
     @Test
     public void addCourseSelect() {
-        ResponseEntity response = courseSelectService.addCourseSelect(testUsername, testCourseCode);
+        ResponseEntity response = courseSelectService.addCourseSelect(TEST_USERNAME, TEST_COURSE_CODE);
         log.info(response.getMessage().toString());
         assertEquals(STATUS_OK, response.getStatus());
 
-        ResponseEntity response2 = courseSelectService.addCourseSelect(testUsername, testCourseCode);
+        ResponseEntity response2 = courseSelectService.addCourseSelect(TEST_USERNAME, TEST_COURSE_CODE);
         assertEquals(STATUS_ERROR, response2.getStatus());
 
-        ResponseEntity responseCurriculum = courseSelectService.getCurriculum(testUsername);
+        ResponseEntity responseCurriculum = courseSelectService.getCurriculum(TEST_USERNAME);
         assertEquals(MESSAGE_OK, responseCurriculum.getMessage().toString());
         log.info(responseCurriculum.getBody().toString());
-        assertTrue(responseCurriculum.getBody().toString().contains(testCourseCode));
+        assertTrue(responseCurriculum.getBody().toString().contains(TEST_COURSE_CODE));
         assertEquals(STATUS_OK, responseCurriculum.getStatus());
     }
 
     @Test
     public void deleteCourseSelected() {
-        ResponseEntity response = courseSelectService.addCourseSelect(testUsername, testCourseCode);
+        ResponseEntity response = courseSelectService.addCourseSelect(TEST_USERNAME, TEST_COURSE_CODE);
         assertEquals(MESSAGE_OK, response.getMessage().toString());
         assertEquals(STATUS_OK, response.getStatus());
-        ResponseEntity responseDelete = courseSelectService.deleteCourseSelected(testCourseCode, testUsername);
+        ResponseEntity responseDelete = courseSelectService.deleteCourseSelected(TEST_COURSE_CODE, TEST_USERNAME);
         assertEquals(MESSAGE_OK, responseDelete.getMessage().toString());
         assertEquals(STATUS_OK, responseDelete.getStatus());
     }
@@ -117,89 +117,89 @@ public class CourseSelectServiceImplTest {
     @Test
     public void getCurriculum() {
 
-        ResponseEntity responseAdd = courseSelectService.addCourseSelect(testUsername, testCourseCode);
+        ResponseEntity responseAdd = courseSelectService.addCourseSelect(TEST_USERNAME, TEST_COURSE_CODE);
         log.info(responseAdd.getMessage().toString());
         assertEquals(STATUS_OK, responseAdd.getStatus());
 
-        ResponseEntity responseAdd2 = courseSelectService.addCourseSelect(testUsername, testCourseCode);
+        ResponseEntity responseAdd2 = courseSelectService.addCourseSelect(TEST_USERNAME, TEST_COURSE_CODE);
         log.info(responseAdd2.getMessage().toString());
         assertEquals(STATUS_ERROR, responseAdd2.getStatus());
 
-        ResponseEntity responseCurriculum = courseSelectService.getCurriculum(testUsername);
+        ResponseEntity responseCurriculum = courseSelectService.getCurriculum(TEST_USERNAME);
         assertEquals(MESSAGE_OK, responseCurriculum.getMessage().toString());
         log.info(responseCurriculum.getBody().toString());
         assertEquals(STATUS_OK, responseCurriculum.getStatus());
-        assertTrue(responseCurriculum.getBody().toString().contains(testCourseCode));
+        assertTrue(responseCurriculum.getBody().toString().contains(TEST_COURSE_CODE));
     }
 
     @Test
     public void getSelectedCourseCodesByUsername() {
-        ResponseEntity responseAdd = courseSelectService.addCourseSelect(testUsername, testCourseCode);
+        ResponseEntity responseAdd = courseSelectService.addCourseSelect(TEST_USERNAME, TEST_COURSE_CODE);
         assertEquals(MESSAGE_OK, responseAdd.getMessage().toString());
         log.info(responseAdd.getMessage().toString());
         assertEquals(STATUS_OK, responseAdd.getStatus());
 
-        ResponseEntity responseGetCourseCode = courseSelectService.getSelectedCourseCodesByUsername(testUsername);
+        ResponseEntity responseGetCourseCode = courseSelectService.getSelectedCourseCodesByUsername(TEST_USERNAME);
         assertEquals(STATUS_OK, responseGetCourseCode.getStatus());
-        assertTrue(responseGetCourseCode.getBody().toString().contains(testCourseCode));
+        assertTrue(responseGetCourseCode.getBody().toString().contains(TEST_COURSE_CODE));
         assertEquals(MESSAGE_OK, responseGetCourseCode.getMessage().toString());
         log.info(responseGetCourseCode.getBody().toString());
     }
 
     @Test
     public void getSelectedCourses() {
-        ResponseEntity responseAdd = courseSelectService.addCourseSelect(testUsername, testCourseCode);
+        ResponseEntity responseAdd = courseSelectService.addCourseSelect(TEST_USERNAME, TEST_COURSE_CODE);
         assertEquals(MESSAGE_OK, responseAdd.getMessage().toString());
         log.info(responseAdd.getMessage().toString());
         assertEquals(STATUS_OK, responseAdd.getStatus());
 
-        ResponseEntity response = courseSelectService.getSelectedCourses(testUsername);
+        ResponseEntity response = courseSelectService.getSelectedCourses(TEST_USERNAME);
         assertEquals(STATUS_OK, response.getStatus());
-        assertTrue(response.getBody().toString().contains(testCourseCode));
+        assertTrue(response.getBody().toString().contains(TEST_COURSE_CODE));
         assertEquals(MESSAGE_OK, response.getMessage().toString());
         log.info(response.getBody().toString());
     }
 
     @Test
     public void getStudentInfoByCourseCode() {
-        ResponseEntity responseAdd = courseSelectService.addCourseSelect(testUsername, testCourseCode);
+        ResponseEntity responseAdd = courseSelectService.addCourseSelect(TEST_USERNAME, TEST_COURSE_CODE);
         assertEquals(MESSAGE_OK, responseAdd.getMessage().toString());
         log.info(responseAdd.getMessage().toString());
         assertEquals(STATUS_OK, responseAdd.getStatus());
 
-        ResponseEntity response = courseSelectService.getStudentInfoByCourseCode(testCourseCode);
+        ResponseEntity response = courseSelectService.getStudentInfoByCourseCode(TEST_COURSE_CODE);
         assertEquals(STATUS_OK, response.getStatus());
-        assertTrue(response.getBody().toString().contains(testUsername));
+        assertTrue(response.getBody().toString().contains(TEST_USERNAME));
         assertEquals(MESSAGE_OK, response.getMessage().toString());
         log.info(response.getBody().toString());
     }
 
     @Test
     public void getStudentGradesByUsername() {
-        ResponseEntity responseAdd = courseSelectService.addCourseSelect(testUsername, testCourseCode);
+        ResponseEntity responseAdd = courseSelectService.addCourseSelect(TEST_USERNAME, TEST_COURSE_CODE);
         assertEquals(MESSAGE_OK, responseAdd.getMessage().toString());
         log.info(responseAdd.getMessage().toString());
         assertEquals(STATUS_OK, responseAdd.getStatus());
 
-        ResponseEntity response = courseSelectService.getStudentGradesByUsername(testUsername);
+        ResponseEntity response = courseSelectService.getStudentGradesByUsername(TEST_USERNAME);
         assertEquals(STATUS_ERROR, response.getStatus());
     }
 
     @Test
     public void updateStudentGrades() {
 
-        ResponseEntity responseAdd = courseSelectService.addCourseSelect(testUsername, testCourseCode);
+        ResponseEntity responseAdd = courseSelectService.addCourseSelect(TEST_USERNAME, TEST_COURSE_CODE);
         assertEquals(MESSAGE_OK, responseAdd.getMessage().toString());
         log.info(responseAdd.getMessage().toString());
         assertEquals(STATUS_OK, responseAdd.getStatus());
 
-        ResponseEntity responseAdd2 = courseSelectService.addCourseSelect(testUsername, testCourseCode);
+        ResponseEntity responseAdd2 = courseSelectService.addCourseSelect(TEST_USERNAME, TEST_COURSE_CODE);
         log.info(responseAdd2.getMessage().toString());
         assertEquals(STATUS_ERROR, responseAdd2.getStatus());
 
         GradesInfoBO gradesInfo = new GradesInfoBO();
-        gradesInfo.setCourseCode(testCourseCode);
-        gradesInfo.setStudentId(testUsername);
+        gradesInfo.setCourseCode(TEST_COURSE_CODE);
+        gradesInfo.setStudentId(TEST_USERNAME);
         gradesInfo.setGrade(90.0);
 
         ResponseEntity response2 = courseSelectService.updateStudentGrades(gradesInfo);
@@ -215,7 +215,15 @@ public class CourseSelectServiceImplTest {
 
     @Test
     public void getConflictedCourseCode() {
-//        String conflicted = courseSelectService.getConflictedCourseCode(testCourseCode, testUsername);
-//        assertEquals(CourseConflictConst.NO_COURSE_SCHEDULE_CONFLICT);
+        String conflicted = courseSelectService.getConflictedCourseCode(TEST_COURSE_CODE, TEST_USERNAME);
+        assertEquals(CourseConflictConst.NO_COURSE_SCHEDULE_CONFLICT, conflicted);
+
+        ResponseEntity responseAdd = courseSelectService.addCourseSelect(TEST_USERNAME, TEST_COURSE_CODE);
+        assertEquals(MESSAGE_OK, responseAdd.getMessage().toString());
+        log.info(responseAdd.getMessage().toString());
+        assertEquals(STATUS_OK, responseAdd.getStatus());
+
+        conflicted = courseSelectService.getConflictedCourseCode(TEST_CONFLICTED_COURSE_CODE, TEST_USERNAME);
+        assertNotEquals(CourseConflictConst.NO_COURSE_SCHEDULE_CONFLICT, conflicted);
     }
 }
